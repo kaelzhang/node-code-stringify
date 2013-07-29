b78794ce47dc2ec59b6b18353cb4b4c622b67c4a
Kael <i@kael.me>
2013-07-29 18:31:22 +0800

    complete all functionalities; update package.json;

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code-this
 
-Code-this is node.js module that converts JavaScript variables into source code. 
+Code-this is node.js module that converts JavaScript variables into source codes. 
 
 Unlike `JSON.stringify`, code-this also deals with reference(object) types of variables.
 
@@ -14,7 +14,55 @@ npm install code-this --save
 
 ```js
 var code = require('code-this');
-var a = {a: function(n){return n;}};
+var a = {a: function(n){return n;}, b: 1, c: 3};
 
-code.convert(a);
+code(a, null, 2); // 1
 ```
+
+Expression {1} will return:
+
+```js
+'{\n  \'a\': function (n){return n;},\n  \'b\': 1,\n  \'c\': 3\n}'
+```
+
+So you can use code-this to save your javascript variables into a file:
+
+```js
+require('fs').writeFileSync(
+	'output.js', 
+	'module.exports = ' + code(a, null, 2) + ';';
+);
+```
+
+Then 'output.js' will be look like:
+
+```js
+module.exports = {
+  'a': function (n){return n;},
+  'b': 1,
+  'c': 3
+};
+```
+
+## code(subject, replacer, space)
+
+##### subject `mixed`
+
+The subject to be converted
+
+##### replacer `function(key, value)`
+
+The `replacer` argument act just like the second parameter of `JSON.stringify`.
+
+##### space `number`
+
+The `space` argument act just like the third parameter of `JSON.stringify`.
+
+
+## Known Issues
+
+- Code-this can't deal with recursive object or array SO FAR.
+- `space` parameter could not affect the code indent inside functions.
+- Code-this could not deal with variable scope so far.
+
+Those issues or tasks which should be done to enhance the module might be fixed in the future. Or there will be a million thanks if you fork and contribute ~~
--- index.js
+++ index.js
@@ -1,2 +1,157 @@
+
 'use strict';
 
+var node_util = require('util');
+
+var QUOTE = '\'';
+var PRE_CURLY_BRANKET = '{';
+var SUF_CURLY_BRANKET = '}';
+var PRE_BRANKET = '[';
+var SUF_BRANKET = ']';
+
+
+function create_spaces(n){
+    if(!n){
+        return '';
+    }
+
+    var space = ' ';
+    var ret = '';
+
+    while(n --){
+        ret += space;
+    }
+
+    return ret;
+}
+
+
+// @param {Object} object
+// @param {function|null} replacer same as `replacer` parameter of `JSON.stringify`
+// @param {number} space same as `space` parameter of `JSON.stringify`
+// @param {number} indent
+function object_to_code(object, replacer, space, indent) {
+    space = space || 0;
+    indent = indent || 0;
+
+    var key;
+    var value;
+
+    var indent_string = create_spaces(indent);
+    var joiner = (space ? '\n' + create_spaces(space) : '') + indent_string;
+    var key_value_joiner = space ? ' ' : '';
+
+    var start = PRE_CURLY_BRANKET + joiner;
+    var end = (space ? '\n' : '') + indent_string + SUF_CURLY_BRANKET;
+    var code = [];
+
+    joiner = ',' + joiner;
+
+    for(key in object){
+        if( object.hasOwnProperty(key) ){
+            value = object[key];
+
+            if(replacer){
+                value = replacer(key, value);
+
+                if(value === undefined){
+                    continue;
+                }
+            }
+
+            code.push(
+                string_to_code(key) + 
+                ':' + key_value_joiner + 
+                value_to_code(value, replacer, space, space + indent) 
+            );
+        }
+    }
+
+    code = code.join(joiner);
+
+    return code ? start + code + end : {};
+};
+
+
+function string_to_code(string){
+    return QUOTE + string.replace(/'/g, '\\\'') + QUOTE;
+};
+
+
+function value_to_code(value, replacer, space, indent) {
+    var type = typeof value;
+
+    if(value === undefined){
+        return 'undefined';
+    
+    }else if(value === null){
+        return 'null';
+
+    }else if(type === 'number'){
+        return value;
+
+    }else if(type === 'string'){
+        return string_to_code(value);
+    
+    }else if(type === 'boolean'){
+        return value ? 'true' : 'false';
+    
+    }else if(type === 'function' || node_util.isRegExp(value)){
+        return value.toString();
+    
+    }else if(node_util.isArray(value)){
+        return array_to_code(value, replacer, space, indent);
+    
+    }else{
+        return object_to_code(value, replacer, space, indent);
+    }
+}
+
+
+
+// @param {Array} array
+function array_to_code(array, replacer, space, indent){
+    space = space || 0;
+    indent = indent || 0;
+
+    var key;
+    var value;
+
+    var indent_string = create_spaces(indent);
+    var joiner = (space ? '\n' + create_spaces(space) : '') + indent_string;
+
+    var start = PRE_BRANKET + joiner;
+    var end = (space ? '\n' : '') + indent_string + SUF_BRANKET;
+    var code = [];
+
+    joiner = ',' + joiner;
+
+    var i = 0;
+    var length = array.length;
+    var value;
+
+    // Never use any iterators of Array, such as .reduce(), .forEach(), etc,
+    // 'coz those method will never iterate unset items of array
+    for(; i < length; i ++){
+        value = array[i];
+
+        if(replacer){
+            value = replacer( String(i), value );
+
+            if(value === undefined){
+
+                // If `replacer` returns undefined then null is used instead, as well as `JSON.stringify`
+                value = null;
+            }
+        }
+
+        code.push( value_to_code(value, replacer, space, indent + space) );
+    }
+
+    code = code.join(joiner);
+
+    return code ? start + code + end : [];
+}
+
+
+module.exports = value_to_code;
